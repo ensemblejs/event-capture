85b1ef496e5bfc8910b893f1fab5c8348090d930
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-07-21 08:28:21 +1000

    add logging and 404 reporting

--- src/server.js
+++ src/server.js
@@ -4,24 +4,46 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var uuid = require('node-uuid');
 var fs = require('fs');
+var logger = require('./logger').logger;
+var expressBunyanLogger = require('express-bunyan-logger');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressBunyanLogger({
+  logger: logger,
+  excludes: [
+    'req',
+    'res',
+    'res-headers',
+    'response-hrtime',
+    'short-body',
+    'req-headers',
+    'incoming',
+    'req_id'
+  ]
+}));
+app.use(expressBunyanLogger.errorLogger({
+  logger: logger
+}));
+
 
 var port = process.env.PORT || 3000;
 
-fs.mkdir('events', function () {});
-
-function saveEvent (request, response) {
+function saveEvent (req, res) {
   var filename = 'events/' + uuid() + '.json';
-  var content = request.body;
+  var content = req.body;
 
   fs.writeFile(filename, JSON.stringify(content));
-  response.sendStatus(200);
+  res.sendStatus(200);
+}
+
+function respond404 (req, res) {
+  res.sendStatus(404);
 }
 
 app.post('/event', saveEvent);
+app.get('*', respond404);
 
 app.listen(port, function () {
   var versionInfo = require('../package.json');
